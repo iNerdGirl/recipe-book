4f76b5542373e75f360b2ad022784f8322923774
Autumn Raine <autumn@inerdgirl.com>
2019-10-31 11:46:11 -0600

    Changed some file structure 🥳

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { ShoppingListComponent } from './shopping/shopping-list/shopping-list.component';
-import { EditShoppingListComponent } from './shopping/edit-shopping-list/edit-shopping-list.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { EditShoppingListComponent } from './shopping-list/edit-shopping-list/edit-shopping-list.component';
 import { RecipeListComponent } from './recipe-book/recipe-list/recipe-list.component';
 import { RecipeComponent } from './recipe-book/recipe.component';
 import { RecipeItemComponent } from './recipe-book/recipe-list/recipe-item/recipe-item.component';
